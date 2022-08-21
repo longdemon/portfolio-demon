2bfffc55fe29fca9214aa4bb781b526817411ac1
longdemon <longnv031120@gmail.com>
2022-08-21 15:54:24 +0700

    Update portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,43 +1,51 @@
 import React from "react";
 import "./portfolio.css";
 import IMG1 from "../../assets/image/background_tmp.jpg";
+import FLRBMS from "../../assets/image/FLRBMS.png";
+import Halloween from "../../assets/image/Halloween.png";
 
 const data = [
   {
     id: 1,
-    image: IMG1,
-    title: "Update soon",
-    github: "https://github.com",
+    image: FLRBMS,
+    title: "FPTU Library Room Booking Manager System",
+    github: "private",
+    demo: null,
   },
   {
     id: 2,
-    image: IMG1,
-    title: "Update soon",
-    github: "https://github.com",
+    image: Halloween,
+    title: "Halloween web",
+    github: "https://github.com/longdemon/Halloween-web",
+    demo: "https://longdemon.github.io/Halloween-web/",
   },
   {
     id: 3,
     image: IMG1,
     title: "Update soon",
     github: "https://github.com",
+    demo: null,
   },
   {
     id: 4,
     image: IMG1,
     title: "Update soon",
     github: "https://github.com",
+    demo: null,
   },
   {
     id: 5,
     image: IMG1,
     title: "Update soon",
     github: "https://github.com",
+    demo: null,
   },
   {
     id: 6,
     image: IMG1,
     title: "Update soon",
     github: "https://github.com",
+    demo: null,
   },
 ];
 
@@ -52,22 +60,37 @@ const Portfolio = () => {
       </h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github }) => {
+        {data.map(({ id, image, title, github, demo }) => {
           return (
             <article className="portfolio__item" key={id}>
               <div className="portfolio__item-image">
                 <img src={image} alt="" />
               </div>
-              <h3>{title}</h3>
+              <h3 className="portfolio__title">{title}</h3>
               <div className="portfolio__item-cta">
-                <a
-                  href={github}
-                  className="btn"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Github
-                </a>
+                {github === "private" ? (
+                  <p className="btn-disable">Private</p>
+                ) : (
+                  <a
+                    href={github}
+                    className="btn"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Github
+                  </a>
+                )}
+
+                {demo ? (
+                  <a
+                    href={demo}
+                    className="btn"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Demo
+                  </a>
+                ) : null}
               </div>
             </article>
           );
